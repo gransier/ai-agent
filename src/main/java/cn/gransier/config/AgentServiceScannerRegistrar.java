b9a6282c5d5c005bac9e1070941aee8ef773c120
libin <15280005979@163.com>
2026-03-13 10:10:31 +0800

    AgentServiceScannerRegistrar修复扫描机制，支持jar包扫描

--- src/main/java/cn/gransier/config/AgentServiceScannerRegistrar.java
+++ src/main/java/cn/gransier/config/AgentServiceScannerRegistrar.java
@@ -7,68 +7,90 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 
-import java.io.File;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 public class AgentServiceScannerRegistrar implements ImportBeanDefinitionRegistrar {
 
+    private static final String RESOURCE_PATTERN = "/**/*.class";
+
     @Override
-    public void registerBeanDefinitions(@NonNull AnnotationMetadata importingClassMetadata, @NonNull BeanDefinitionRegistry registry) {
-        List<Class<?>> mapperClasses = getMapperClasses(importingClassMetadata);
-        for (Class<?> mapperClass : mapperClasses) {
-            registerMapperBeanDefinition(mapperClass, registry);
+    public void registerBeanDefinitions(@NonNull AnnotationMetadata importingClassMetadata,
+                                        @NonNull BeanDefinitionRegistry registry) {
+        Map<String, Object> attributes = importingClassMetadata.getAnnotationAttributes(AgentServiceScan.class.getName());
+        if (attributes == null) {
+            return;
+        }
+
+        String[] basePackages = (String[]) attributes.get("value");
+        if (basePackages == null || basePackages.length == 0) {
+            // 默认扫描注解所在类的包
+            basePackages = new String[]{ClassUtils.getPackageName(importingClassMetadata.getClassName())};
+        }
+
+        List<Class<?>> agentInterfaces = scanForAgentInterfaces(basePackages);
+        for (Class<?> clazz : agentInterfaces) {
+            registerAgentServiceBean(clazz, registry);
         }
     }
 
-    private List<Class<?>> getMapperClasses(AnnotationMetadata metadata) {
-        List<Class<?>> classes = new ArrayList<>();
-        Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(AgentServiceScan.class.getName());
-        assert annotationAttributes != null;
-        String[] basePackages = (String[]) annotationAttributes.get("value");
+    private List<Class<?>> scanForAgentInterfaces(String[] basePackages) {
+        List<Class<?>> result = new ArrayList<>();
+        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
 
         for (String basePackage : basePackages) {
-            classes.addAll(scanPackage(basePackage));
-        }
-        return classes;
-    }
+            String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +
+                    ClassUtils.convertClassNameToResourcePath(basePackage) + RESOURCE_PATTERN;
 
-    private List<Class<?>> scanPackage(String basePackage) {
-        List<Class<?>> classes = new ArrayList<>();
-        String packagePath = basePackage.replace('.', '/');
-        URL url = Thread.currentThread().getContextClassLoader().getResource(packagePath);
-        if (url != null && "file".equals(url.getProtocol())) {
-            File dir = new File(url.getPath());
-            for (File file : Objects.requireNonNull(dir.listFiles())) {
-                if (file.isDirectory()) {
-                    classes.addAll(scanPackage(basePackage + "." + file.getName()));
-                } else if (file.getName().endsWith(".class")) {
-                    try {
-                        Class<?> clazz = Class.forName(basePackage + "." + file.getName().replace(".class", ""));
-                        AgentService annotation = clazz.getAnnotation(AgentService.class);
-                        if (clazz.isInterface() && annotation != null) {
-                            classes.add(clazz);
+            try {
+                org.springframework.core.io.Resource[] resources = resourceResolver.getResources(packageSearchPath);
+                for (org.springframework.core.io.Resource resource : resources) {
+                    if (!resource.isReadable()) {
+                        continue;
+                    }
+
+                    MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                    String className = metadataReader.getClassMetadata().getClassName();
+
+                    // 只处理接口 + 带 @AgentService 注解
+                    if (metadataReader.getClassMetadata().isInterface()) {
+                        AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
+                        if (annotationMetadata.hasAnnotation(AgentService.class.getName())) {
+                            try {
+                                Class<?> clazz = Class.forName(className);
+                                log.info("Registering agent service interface: {}", clazz.getName());
+                                result.add(clazz);
+                            } catch (ClassNotFoundException e) {
+                                log.warn("Failed to load class: {}", className, e);
+                            }
                         }
-                    } catch (ClassNotFoundException e) {
-                        log.error("Class not found: {}", e.getMessage());
                     }
                 }
+            } catch (IOException e) {
+                log.error("I/O error while scanning package: {}", basePackage, e);
             }
         }
-        return classes;
+        return result;
     }
 
-    private void registerMapperBeanDefinition(Class<?> mapperClass, BeanDefinitionRegistry registry) {
+    private void registerAgentServiceBean(Class<?> mapperClass, BeanDefinitionRegistry registry) {
         String beanName = StringUtils.uncapitalize(mapperClass.getSimpleName());
         BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(AgentServiceFactoryBean.class);
         builder.addPropertyValue("mapperClass", mapperClass);
         registry.registerBeanDefinition(beanName, builder.getBeanDefinition());
+        log.info("Registered agent service bean: {}", beanName);
     }
 }
