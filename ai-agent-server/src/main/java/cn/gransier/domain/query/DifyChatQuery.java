b6aec76934aa3765afe72124175028f772b0a327
libin <15280005979@163.com>
2026-03-19 11:46:58 +0800

    暂时移除oss引用

--- ai-agent-server/src/main/java/cn/gransier/domain/query/DifyChatQuery.java
+++ ai-agent-server/src/main/java/cn/gransier/domain/query/DifyChatQuery.java
@@ -2,6 +2,7 @@ package cn.gransier.domain.query;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModelProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.List;
@@ -15,11 +16,13 @@ public class DifyChatQuery {
     @JsonInclude
     private Map<String, Object> inputs = Map.of();
     @ApiModelProperty("用户输入/提问内容")
+    @NotBlank(message = "用户输入/提问内容不能为空")
     private String query;
 
     @ApiModelProperty("streaming:流式")
     private final String response_mode = "streaming";
     @ApiModelProperty("用户标识")
+    @NotBlank(message = "用户标识不能为空")
     private String user;
     @ApiModelProperty("（选填）会话 ID，需要基于之前的聊天记录继续对话，必须传之前消息的 conversation_id。")
     private String conversation_id;
